f22cbe2b0b06450cf393104cc73e1daf3078f470
Galati Gaetan <skyfulle68@gmail.com>
2021-01-11 11:39:03 +0100

    Strategy
    Strategy permet d'assigner dynamiquement des composants aux objets

---
+++ Assets/Strategy Pattern/ShipController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum WeaponType{
+    Missile,
+    Bullet
+}
+public class ShipController : MonoBehaviour
+{
+    public string current;
+
+    public WeaponType weaponType; 
+    
+    
+    private IWeapon iWeapon;
+    
+    private void HandleWeaponType(){
+     
+        Component c = gameObject.GetComponent<IWeapon>() as Component;
+        if(c!=null){
+            Destroy(c);
+        }
+        
+        switch(weaponType){
+        
+            case WeaponType.Missile:
+                iWeapon = gameObject.AddComponent<Missile> ();
+                current = "Missile";
+                break;
+                
+            case WeaponType.Bullet:
+                iWeapon = gameObject.AddComponent<Bullet> ();
+                current = "Bullet";
+                break;
+                
+            default:
+                iWeapon = gameObject.AddComponent<Bullet> ();
+                break;
+        }
+        
+    }
+      public void Fire(){    
+        iWeapon.Shoot();
+        Debug.Log("Fire !");
+    }
+        void Update () {
+    
+        if (Input.GetKeyDown("space")){
+            Fire();
+        }
+        
+        if(Input.GetKeyDown("a")){
+            HandleWeaponType();
+            Debug.Log("Votre arme est :"+current );        
+        }    
+        
+    }
+
+}
+
---
+++ Assets/Strategy Pattern/WeaponManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+}
+
+public interface IWeapon{
+    void Shoot(); 
+}
+
+public class Bullet: MonoBehaviour, IWeapon{
+    public void Shoot(){
+    
+        Vector3 initialPosition = new Vector3 (this.transform.position.x, this.transform.position.y +1f,0);
+        GameObject bullet =  Instantiate(Resources.Load("Bullet",typeof(GameObject))) as GameObject;    
+        bullet.transform.position = initialPosition;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0f,3f);
+    } 
+}
+
+public class Missile: MonoBehaviour, IWeapon{
+    public void Shoot(){
+        
+        Vector3 initialPosition = new Vector3 (this.transform.position.x, this.transform.position.y + 1f,0);
+        GameObject missile =  Instantiate(Resources.Load("Missile",typeof(GameObject))) as GameObject;    
+        missile.transform.position = initialPosition;
+        missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0f,3f);
+        
+    }
+}
